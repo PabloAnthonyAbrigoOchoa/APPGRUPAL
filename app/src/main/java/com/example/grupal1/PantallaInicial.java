5e756c61edf947e675090e0323eb5ab4312309b3
Pablo Abrigo Ochoa <paabrigo@tecnologicoloja.edu.ec>
2021-07-10 01:24:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/grupal1/PantallaInicial.java
@@ -0,0 +1,45 @@
+package com.example.grupal1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PantallaInicial extends AppCompatActivity {
+    private Button Regresar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_inicial);
+        Regresar=(Button)findViewById(R.id.RegresarMenu);
+        Regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PantallaInicial.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
