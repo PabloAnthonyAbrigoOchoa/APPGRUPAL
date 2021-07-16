d245a178800b80b1dbc377e9bf0cd4bf9eb652bb
Pablo Abrigo Ochoa <paabrigo@tecnologicoloja.edu.ec>
2021-07-16 01:29:05 -0500

    Rango y Toast!

--- app/src/main/java/com/example/grupal1/MainActivity.java
+++ app/src/main/java/com/example/grupal1/MainActivity.java
@@ -6,66 +6,126 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     private Button btnJugar;
-    private TextView txtcorrecto;
-    private TextView numeroIngresado;
-    private int aleatorio;
-    private int intentos;
+    private TextView txtadivina;
+    private TextView txtrango;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtcorrecto=(TextView) findViewById(R.id.edittxtAdivina);
-        btnJugar=(Button) findViewById(R.id.btncambiarPantallaInicial);
-        aleatorio=generarNumerosAleatorios();
-        btnJugar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, PantallaInicial.class);
-                startActivity(i);
-                int numeroUsuario;
-                numeroUsuario=Integer.parseInt(numeroIngresado.getText().toString());
-                if (aleatorio==numeroUsuario){
-                    numeroIngresado.setText("Has Ganado, eres un Crack :')");
-                }else{
-                    intentos=intentos-1;
-                }
-                if(intentos==0){
-                    btnJugar.setEnabled(false);
-                    numeroIngresado.setText("QUE PENA, HAS PERDIDO :'(");
+        txtadivina=(TextView) findViewById(R.id.edittxtAdivina);
+        txtrango=(TextView) findViewById(R.id.editTxtRango);
+        btnJugar=(Button) findViewById(R.id.btnJugar);
+        btnJugar.setOnClickListener(this);
+    }
+        @Override
+        public void onClick(View v) {
+
+            if(v==btnJugar){
+                if(txtadivina.getText().toString().isEmpty()){
+                    txtadivina.setError("Escriba un número");
+                } else{
+                    if(txtrango.getText().toString().isEmpty()){
+                        txtrango.setError("Escriba el Rango a jugar");
+                    } else{
+                        try {
+                            int numTxt = Integer.valueOf(txtadivina.getText().toString());
+                            int txtRangoObtenido = Integer.valueOf(txtrango.getText().toString());
+                            if (numTxt>txtRangoObtenido){
+                                txtadivina.setError("Ingrese un número dentro del Rango que establecio.");
+
+                            }else{
+                                Verificar();
+                                /*btnJugar.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Intent i = new Intent(MainActivity.this, PantallaInicial.class);
+                                        startActivity(i);
+                                    }
+                                });*/
+                            }
+                }catch (NumberFormatException n){
+
+                        }
+
+                    }
                 }
             }
-        });
 
-    }
+        }
 
-    public int generarNumerosAleatorios(){
-        return (int)(Math.random()*10+1);
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
+        public int generarNumerosAleatorios(int a){
+            Random naleatorio = new Random();
+            int n= naleatorio.nextInt(a);
+            return n;
+        }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
+        public void Verificar(){
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+            try {
+                int numTxt = Integer.valueOf(txtadivina.getText().toString());
+                int txtRangoObtenido = Integer.valueOf(txtrango.getText().toString());
+                int numAleatorio = generarNumerosAleatorios(txtRangoObtenido);
+                if(numTxt==numAleatorio){
+                    Toast.makeText(this,"GANASTE, ERES EXCELENTE :-)",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(this,":-( LO SENTIMOS, PERDISTE. El Número era: "+numAleatorio,Toast.LENGTH_LONG).show();
+                }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+            }catch(NumberFormatException n){
+                txtadivina.setError("Ingrese solamente números");
+            }
 
+        }
+        /*public String RespuestaVerificar(){
+
+        try {
+            int numTxt = Integer.valueOf(txtadivina.getText().toString());
+            int txtRangoObtenido = Integer.valueOf(txtrango.getText().toString());
+            int numAleatorio = generarNumerosAleatorios(txtRangoObtenido);
+            if(numTxt==numAleatorio){
+                String Ganaste = "GANASTE, ERES EXCELENTE :-)";
+                return Ganaste;
+            }else{
+                String Perdiste = ":-( LO SENTIMOS, PERDISTE. El Número era:"+numAleatorio;
+                return Perdiste;
+            }
+
+        }catch(NumberFormatException n){
+
+        }
+        return "Ocurrió un error";
+
+    }*/
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+        }
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+        }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+        }
 }
+
+
--- app/src/main/java/com/example/grupal1/PantallaInicial.java
+++ app/src/main/java/com/example/grupal1/PantallaInicial.java
@@ -4,20 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.grupal1.MainActivity;
 
 public class PantallaInicial extends AppCompatActivity {
 
 
     private Button SigaIntentando;
-
+    private TextView txtCorrecto;
+    private MainActivity mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pantalla_inicial);
         SigaIntentando=(Button)findViewById(R.id.btnRegresarMenuP);
+        txtCorrecto=(TextView) findViewById(R.id.txttxtcorrecto);
+        /*txtCorrecto.setText(mainActivity.RespuestaVerificar());*/
         SigaIntentando.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,4 +50,10 @@ public class PantallaInicial extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
 }
